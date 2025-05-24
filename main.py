e68d030528167a0b68e96c8ef8e31414e6eb5103
Nozomi <nozomigiladi@gmail.com>
2025-05-24 22:20:38 +1000

    Fixing yfinance

---
+++ main.py
@@ -0,0 +1,8 @@
+import xgboost as xg
+import yfinance as yf
+import matplotlib as mpl
+import arch as a
+import sys
+import numpy as np
+import math
+print(sys.argv)
--- project.py
+++ project.py
@@ -1,1 +1,7 @@
-
+import xgboost as xg
+import yfinance as yf
+import matplotlib as mpl
+import arch as a
+import sys
+import numpy as np
+import math
